b98964094395ad16c7aa1f3766d57c721c1fae5f
s0nders <53185073+s0nders@users.noreply.github.com>
2022-03-30 08:52:45 -0400

    Test

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 # The configuration for the `remote` backend.
-  terraform {
-    backend "remote" {
-      # The name of your Terraform Cloud organization.
-      organization = "GosuNinja"
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "GosuNinja"
 
-      # The name of the Terraform Cloud workspace to store Terraform state files in.
-      workspaces {
-        name = "website"
-      }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "prod_gs_website"
     }
   }
+}
 
-  # An example resource that does nothing.
-  resource "null_resource" "example" {
-    triggers = {
-      value = "A example resource that does nothing!"
-    }
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
   }
+}
